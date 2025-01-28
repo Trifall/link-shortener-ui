875335b922b4073487b176fb174f0ee86fdd21f1
Trifall <JerrenT@Trifall.com>
2025-01-27 20:38:04 -0500

    Refactor layout structure: enhance Toast positioning and wrap main content in a relative container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 	import { fly } from 'svelte/transition';
 
 	import { GlobalKeyState, IsValidKeyState } from '$lib/KeyState.svelte';
-	import LoginPanel from '@components/LoginPanel.svelte';
 	import Dashboard from '@components/Dashboard.svelte';
 	import Footer from '@components/Footer.svelte';
+	import LoginPanel from '@components/LoginPanel.svelte';
 
 	let showToast = $state(false);
 
@@ -22,34 +22,35 @@
 	}
 </script>
 
-<Toast
-	transition={fly}
-	params={{ x: 200 }}
-	position={'bottom-right'}
-	color="blue"
-	class="mb-4 !rounded-xl !text-white"
-	bind:toastStatus={showToast}
-	on:close={() => {
-		showToast = false;
-	}}
-	defaultIconClass="!bg-transparent"
->
-	<Check slot="icon" class="h-6 w-6 bg-transparent !stroke-green-500" />
-	<span>Successfully logged in!</span>
-</Toast>
-
-<div class="flex min-h-[80vh] items-center justify-center bg-gray-900 text-white">
-	<div class="flex flex-col items-center justify-center gap-2 bg-gray-900 text-white">
-		{#key GlobalKeyState.key}
-			{#if IsValidKeyState()}
-				<!-- Dashboard Layout -->
-				<Dashboard />
-			{:else}
-				<!-- Existing Login UI -->
-				<LoginPanel {showToastHandler} />
-			{/if}
-		{/key}
+<div class="relative overflow-x-hidden">
+	<Toast
+		transition={fly}
+		params={{ x: 200 }}
+		position={'bottom-right'}
+		color="blue"
+		class="!z-[9999] mb-4 !rounded-xl !text-white"
+		bind:toastStatus={showToast}
+		on:close={() => {
+			showToast = false;
+		}}
+		defaultIconClass="!bg-transparent"
+	>
+		<Check slot="icon" class="h-6 w-6 bg-transparent !stroke-green-500" />
+		<span>Successfully logged in!</span>
+	</Toast>
+	<div class="flex min-h-[80vh] max-w-full items-center justify-center bg-gray-900 text-white">
+		<div class="flex flex-col items-center justify-center gap-2 bg-gray-900 text-white">
+			{#key GlobalKeyState.key}
+				{#if IsValidKeyState()}
+					<!-- Dashboard Layout -->
+					<Dashboard />
+				{:else}
+					<!-- Existing Login UI -->
+					<LoginPanel {showToastHandler} />
+				{/if}
+			{/key}
+		</div>
 	</div>
-</div>
 
-<Footer />
+	<Footer />
+</div>
