a542b05bad709467d0e9a0310fba229678f2351e
Trifall <JerrenT@Trifall.com>
2025-02-02 05:59:55 -0500

    Refactor +page.svelte: streamline ToastWrapper usage and adjust layout classes for improved responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,12 @@
 	import ToastWrapper from '@components/ToastWrapper.svelte';
 </script>
 
+<ToastWrapper />
 <div class="relative flex min-h-screen flex-col overflow-x-hidden">
-	<ToastWrapper />
-
 	<div
-		class="mt-24 flex min-h-[80vh] max-w-full items-center justify-center bg-gray-900 text-white"
+		class={`flex min-h-[80vh] max-w-full items-center justify-center bg-gray-900 text-white ${IsValidKeyState() ? 'mt-24' : ''}`}
 	>
-		<div class="flex flex-col items-center justify-center gap-2 bg-gray-900 text-white">
+		<div class="flex flex-col items-center justify-start gap-2 bg-gray-900 text-white">
 			{#key GlobalKeyState.key}
 				{#if IsValidKeyState()}
 					<!-- Dashboard Layout -->
